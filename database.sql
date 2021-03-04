c56e3fa260f49ce6c78b7bd7daae2488faac03cd
pablo0899 <70744303+pablo0899@users.noreply.github.com>
2021-03-03 20:14:04 -0600

    Add files via upload

--- database.sql
+++ database.sql
@@ -6,9 +6,9 @@ create table Alumno(
     `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `usuario` varchar(32) NOT NULL,
     `contrasena` varchar(32) NOT NULL,
-    `puntuacion` float,
-    `correctas` int,
-    `incorrectas` int 
+    `puntuacion` float NOT NULL,
+    `correctas` int NOT NULL,
+    `incorrectas` int NOT NULL
 );
 
 create table Examen(
@@ -22,9 +22,3 @@ create table Examen(
     `opcionE` varchar(64),
     `justificacion` varchar(128) 
 );
-
-INSERT INTO examen (area,pregunta,opcionA, opcionB, opcionC, opcionD, opcionE, justificacion) 
-VALUES ('Programacion','Es un lenguaje orientado a objetos','C++','Python','PHP','Pearl','Ninguno de los anteriores','C++ es un lenguaje orientado a objetos, ya que permite trabajar con Entidades (Objetos)');
-
-INSERT INTO examen (area,pregunta,opcionA, opcionB, opcionC, opcionD, opcionE, justificacion) 
-VALUES ('Bases de datos','Sentencia para ingresar datos a una tabla','INSERT INTO','ALTER TABLE','INSERT','DROP','SELECT','INSERT INTO es la sentencia utilizada para ingresar datos a una base de datos');
