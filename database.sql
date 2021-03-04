57ad7accc292f3e90c685a1dea1463b951598eb7
pablo0899 <70744303+pablo0899@users.noreply.github.com>
2021-03-03 18:19:28 -0600

    editar BD

--- database.sql
+++ database.sql
@@ -1,24 +1,47 @@
-drop database if exists ceneval;
-create database ceneval;
-use ceneval;
+--
+-- Base de datos: `ceneval`
+--
+DROP DATABASE IF EXISTS `ceneval`;
+CREATE DATABASE `ceneval`;
 
-create table Alumno(
-    `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `usuario` varchar(32) NOT NULL,
-    `contrasena` varchar(32) NOT NULL,
-    `puntuacion` float NOT NULL,
-    `correctas` int NOT NULL,
-    `incorrectas` int NOT NULL
-);
+-- --------------------------------------------------------
 
-create table Examen(
-    `id_pregunta` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `area` varchar(32),
-    `pregunta` varchar(64),
-    `opcion A` varchar(64),
-    `opcion B` varchar(64),
-    `opcion C` varchar(64),
-    `opcion D` varchar(64),
-    `opcion E` varchar(64),
-    `justificacion` varchar(128) 
-);
+--
+-- Estructura de tabla para la tabla `alumno`
+--
+
+DROP TABLE IF EXISTS `alumno`;
+CREATE TABLE IF NOT EXISTS `alumno` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(32) NOT NULL,
+  `contrasena` varchar(32) DEFAULT NULL,
+  `puntuacion` float DEFAULT NULL,
+  `correctas` int(11) DEFAULT NULL,
+  `incorrectas` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `examen`
+--
+
+DROP TABLE IF EXISTS `examen`;
+CREATE TABLE IF NOT EXISTS `examen` (
+  `id_pregunta` int(11) NOT NULL AUTO_INCREMENT,
+  `area` varchar(32) DEFAULT NULL,
+  `pregunta` varchar(64) DEFAULT NULL,
+  `opcion A` varchar(64) DEFAULT NULL,
+  `opcion B` varchar(64) DEFAULT NULL,
+  `opcion C` varchar(64) DEFAULT NULL,
+  `opcion D` varchar(64) DEFAULT NULL,
+  `opcion E` varchar(64) DEFAULT NULL,
+  `justificacion` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id_pregunta`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
