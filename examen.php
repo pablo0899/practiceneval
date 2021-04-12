0d4be6e8d76ed31ae5a42abefa87d4eefafa94e9
pablo0899 <70744303+pablo0899@users.noreply.github.com>
2021-04-11 21:07:10 -0500

    Add files via upload

--- examen.php
+++ examen.php
@@ -4,7 +4,7 @@
     <title>PractiCeneval</title>    
     <meta charset="UTF-8">
     <link href="estilos3.css" rel="stylesheet" type="text/css"/>  
-</head>  
+</head> 
 <body id="body_examen">
 <?php
     include("conexion.php");
@@ -17,12 +17,16 @@
     $result = mysqli_query($link,"SELECT COUNT(*) FROM examen");
     $row=mysqli_fetch_array($result);
     $resultado= $row['COUNT(*)'];
-    $num_pregunta = mt_rand ( 1 , $resultado );
+
+    $num_pregunta = mt_rand(1, $resultado);
+
+    $resultado1 = mysqli_query($link,"SELECT respuesta_respondida FROM examen_alumno WHERE id_pregunta  ='$num_pregunta'");
+    $consulta1 = mysqli_fetch_array($resultado1);
+
+    if($consulta1 == NULL){
+
 ?>   
 <nav>
-<p>
-    AQUI VA EL CRONOMETRO
-</p>
 <tr>
     <th colspan="3">Hola <strong><?php echo $user;?></strong>, éxito</th>
 </tr>
@@ -30,7 +34,7 @@
     <div class="cuadro2">
         <form id="formulario_examen" action="examen_validacion.php" method="POST">  
             <table id="table_examen">
-                <?php foreach ($link->query("SELECT * from examen WHERE id_pregunta  ='$num_pregunta' ") as $row) { ?>
+                <?php foreach ($link->query("SELECT * from examen WHERE id_pregunta  =' $num_pregunta' ") as $row) { ?>
                 <tr>
                     <td><p>Pregunta</p></td>
                     <td><?php echo $row['id_pregunta']; ?></td>
@@ -45,7 +49,7 @@
                 </tr>
                 <tr>
                     <td><?php echo $row['opcionA']; ?></td>
-                    <td><input type="radio" name="opcion" id="opc" value="A" autocomplete="off"></td>
+                    <td><input type="radio" name="opcion" id="opc" value="A" autocomplete="off" required></td>
                 </tr>
                 <tr>
                     <td><?php echo $row['opcionB']; ?></td>
@@ -59,18 +63,26 @@
                     <td><?php echo $row['opcionD']; ?></td>
                     <td><input type="radio" name="opcion" id="opc" value="D" autocomplete="off"></td>
                 </tr>
-                <tr>
-                    <td><?php echo $row['opcionE']; ?></td>
-                    <td><input type="radio" name="opcion" id="opc" value="E" autocomplete="off"></td>
-                </tr>
-                <?php }
+                <?php  } }
+                else{
+                    $result1 = mysqli_query($link,"SELECT COUNT(*) FROM examen_alumno");
+                    $row=mysqli_fetch_array($result1);
+                    $resultado1= $row['COUNT(*)'];
+
+                    if($resultado1 >= $resultado){
+                        header("Location:puntuacion.php");
+                    }
+                    else{
+                        header("Location:examen.php");
+                    }
+                }
                 mysqli_close($link);
                 ?>
             </table>
-            <input type="submit" value="Siguiente pregunta" id='boton_validacion' > 
+            <input type="submit" value="Siguiente pregunta" id='boton_validacion'> 
             <br>
             <a href='puntuacion.php'><input type="button" value="Terminar examen" id='boton_validacion'></a>
         </form>    
     </div> 
-</body>
+</body> 
 </html>  
--- examen_validacion.php
+++ examen_validacion.php
@@ -16,21 +16,28 @@
     $user = $_SESSION['user'];
 
     $resultado = mysqli_query($link,"SELECT respuesta_correcta FROM examen WHERE id_pregunta  ='$num_preg' and respuesta_correcta  ='$answer' ");
+    $resultado1 = mysqli_query($link,"SELECT respuesta_respondida FROM examen_alumno WHERE id_pregunta  ='$num_preg'");
     $consulta = mysqli_fetch_array($resultado);
+    $consulta1 = mysqli_fetch_array($resultado1);
 
-    foreach ($link->query("SELECT respuesta_correcta from examen WHERE id_pregunta  = '$num_preg' ") as $row) {
-        $respuesta_correcta =  $row['respuesta_correcta'];
-    } 
-
-    $insertar="INSERT INTO examen_alumno (usuario,id_pregunta,respuesta_respondida,respuesta_correcta) VALUES ('$user','$num_preg','$answer','$respuesta_correcta')";
-    mysqli_query($link,$insertar);
-
-    if($answer == $respuesta_correcta){
-        $correcta= mysqli_query($link,"UPDATE alumno SET correctas = correctas+1 WHERE usuario = '$user' ");
-        header("Location:examen.php");
+    if($consulta1 == NULL){
+        foreach ($link->query("SELECT respuesta_correcta from examen WHERE id_pregunta  = '$num_preg' ") as $row) {
+            $respuesta_correcta =  $row['respuesta_correcta'];
+        } 
+    
+        $insertar="INSERT INTO examen_alumno (usuario,id_pregunta,respuesta_respondida,respuesta_correcta) VALUES ('$user','$num_preg','$answer','$respuesta_correcta')";
+        mysqli_query($link,$insertar);
+    
+        if($answer == $respuesta_correcta){
+            $correcta= mysqli_query($link,"UPDATE alumno SET correctas = correctas+1 WHERE usuario = '$user' ");
+            header("Location:examen.php");
+        }
+        else{
+            $incorrecta=mysqli_query($link,"UPDATE alumno SET incorrectas = incorrectas+1 WHERE usuario = '$user' ");
+            header("Location:examen.php");
+        }
     }
     else{
-        $incorrecta=mysqli_query($link,"UPDATE alumno SET incorrectas = incorrectas+1 WHERE usuario = '$user' ");
         header("Location:examen.php");
     }
     mysqli_close($link);
